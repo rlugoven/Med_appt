b805991f64e5160322b53438942bf499245e544e
rlugoven <rlugoven@ponce.inter.edu>
2024-04-24 17:50:22 -0400

    Add files

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ import Landing_Page from './Components/Landing_Page/Landing_Page';
 import Navbar from './Components/Navbar/Navbar';
 import Sign_Up from './Components/Sign_Up/Sign_Up';
 import Login from './Components/Login/Login';
+import InstantConsultation from './Components/InstantConsultationBooking/InstantConsultation';
+import FindDoctorSearch from './Components/FindDoctorSearch/FindDoctorSearch';
+import BookingConsultation from './Components/BookingConsultation';
+import Notification from './Components/Notification/Notification';
+import ReviewFrom from './Components/ReviewForm/ReviewForm';
+import ProfileCard from './Components/ProfileCard/ProfileCard';
+import ReportsLayout from './Components/ReportsLayout/ReportsLayout';
 
 function App() {
 
@@ -17,7 +24,13 @@ function App() {
                         <Route path="/Landing_Page" element={<Landing_Page />} />
                         <Route path="/Sign_Up" element={<Sign_Up />} />
                         <Route path="/Login" element={<Login />} />
-                                       
+                        <Route path="/instant-consultation" element={<InstantConsultation />} />
+                        <Route path='/finddoctor' element={<FindDoctorSearch />} />
+                        <Route path='/search/doctors' element={<BookingConsultation />} />  
+                        <Route path='/reviews' element={<ReviewFrom />} />
+                        <Route path='/profile' element={<ProfileCard />} />
+                        <Route path='/reports' element={<ReportsLayout />} />  
+               
                 </Routes>
           </Notification>
         </BrowserRouter>
