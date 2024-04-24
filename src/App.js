1304473742e4a069a989fd7465fc6e7d1923f25f
rlugoven <rlugoven@ponce.inter.edu>
2024-04-24 16:58:31 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Landing_Page from './Components/Landing_Page/Landing_Page';
+import Navbar from './Components/Navbar/Navbar';
+import Sign_Up from './Components/Sign_Up/Sign_Up';
+import Login from './Components/Login/Login';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+     return (
+        <div className="App">
+        <BrowserRouter>
+          <Notification>
+            <Navbar />
+              <Routes>
+                        <Route path="/Landing_Page" element={<Landing_Page />} />
+                        <Route path="/Sign_Up" element={<Sign_Up />} />
+                        <Route path="/Login" element={<Login />} />
+                                       
+                </Routes>
+          </Notification>
+        </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
