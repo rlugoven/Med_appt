e8707285c41ccdf5ac393d756d8a812b085f29d1
rlugoven <rlugoven@ponce.inter.edu>
2024-04-25 08:21:56 -0400

    Add files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const express = require('express');
+const cors = require('cors');
+const connectToMongo = require('./db');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 8181;
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Connect to MongoDB
+connectToMongo();
+
+// Routes
+app.use('/api/auth', require('./routes/auth'));
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+  // Start the server
+app.listen(PORT, () => {
+console.log(`Server is running on port http://localhost:${PORT}`);
+});
