dbadb76ee710c46c5f185da956f1f981ed653e7d
rlugoven <rlugoven@ponce.inter.edu>
2024-04-24 22:08:25 -0400

    Add files

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import FindDoctorSearch from './Components/FindDoctorSearch/FindDoctorSearch';
 import BookingConsultation from './Components/BookingConsultation';
 import Notification from './Components/Notification/Notification';
 import ReviewFrom from './Components/ReviewForm/ReviewForm';
+import ProfileCard from './Components/ProfileCard/ProfileCard';
+import ReportsLayout from './Components/ReportsLayout/ReportsLayout';
 
 
 function App() {
@@ -27,6 +29,8 @@ function App() {
                         <Route path='/finddoctor' element={<FindDoctorSearch />} />
                         <Route path='/search/doctors' element={<BookingConsultation />} />  
                         <Route path='/reviews' element={<ReviewFrom />} />
+                        <Route path='/profile' element={<ProfileCard />} />
+                        <Route path='/reports' element={<ReportsLayout />} />  
 
                 </Routes>
           </Notification>
